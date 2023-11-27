24e5c5516004fed0e44cebfdde709aa1480f71c3
tapassre <99166312+tapassre@users.noreply.github.com>
2023-11-27 16:07:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-0efcece6bed30fd98" # us-west-2
+  ami           = "ami-0fc5d935ebf8bc3bc" # us-west-1
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
